0fc4afd905a72db17428e57a75adf69e2a0c5af8
Tim Bradgate <timbradgate@hotmail.co.uk>
2021-04-16 23:40:27 +0100

    DIGI-4: Add node CV action

--- server/server.js
+++ server/server.js
@@ -1,16 +1,21 @@
-const express = require('express');
-const path = require('path');
-const app = express()
-const bodyParser = require("body-parser");
-port = 3080;
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import path from 'path';
 
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(path.resolve(), '../client/dist')));
 
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(path.resolve(), '../client/dist/index.html'));
 });
 
+const port = process.env.PORT || 3080;
 app.listen(port, () => {
-    console.log(`Server listening on the port::${port}`);
+  console.log(`listening on ${port}`);
 });
