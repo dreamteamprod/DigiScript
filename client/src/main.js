7b762d33dda4b0b33b66b850d6da779b9a55cef0
Tim Bradgate <timbradgate@hotmail.co.uk>
2021-04-17 14:27:15 +0100

    Code cleanup

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
   render: h => h(App),
---
+++ vetur.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    settings: {
+      "vetur.useWorkspaceDependencies": true,
+      "vetur.experimental.templateInterpolationService": true
+    },
+    projects: [
+      {
+        root: './client',
+        package: './package.json'
+      }
+    ]
+}
