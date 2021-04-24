24638651d05c882b867778eb83557b423959ba0e
Tim Bradgate <timbradgate@hotmail.co.uk>
2021-04-24 23:05:01 +0100

    DIGI-8: Add basic front end routing (#10)

---
+++ client/src/views/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>DigiScript</h1>
+    <br />
+    <ul class="no-bullets">
+      <li>Tim Bradgate</li>
+      <li>Matthew Stratford</li>
+      <li>Jack Pollock</li>
+      <li>Matt Gaynor</li>
+    </ul>
+    <br />
+    <p>A DreamTeam Productions creation &copy; 2021</p>
+  </div>
+</template>
+
+<style scoped>
+.about {
+  margin: 1em;
+}
+ul.no-bullets {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+</style>
