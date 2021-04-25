1dbac9bb748c67fd2b170c54bf4e89eb6d6b9f8e
Tim Bradgate <timbradgate@hotmail.co.uk>
2021-04-25 16:31:10 +0100

    DIGI-10: Add 404 page (#12)

---
+++ client/src/views/404.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="notfound">
+    <h1>Oh no! 404</h1>
+    <h2>This is not the page you were looking for!</h2>
+    <p>Return <router-link to='/'>home</router-link></p>
+  </div>
+</template>
+
+<style scoped>
+.notfound {
+  margin: 1em;
+}
+</style>
