1169a88fa171eb5b878450ad9f6f2ba74e5408d3
Tim Bradgate <timbradgate@hotmail.co.uk>
2022-07-02 02:02:27 +0200

    Add Vuex store and configure for Websocket

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 import VueNativeSock from 'vue-native-websocket';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 
+import store from '@/store/store';
 import App from './App.vue';
 import router from './router';
 
@@ -14,16 +16,20 @@ Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
 
+Vue.use(Vuex);
+
 Vue.use(VueNativeSock, `ws://${window.location.hostname}:${window.location.port}/api/v1/ws`, {
   reconnection: true,
   reconnectionAttempts: 5,
   reconnectionDelay: 3000,
   format: 'json',
+  store,
 });
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: (h) => h(App),
 }).$mount('#app');
---
+++ client/src/store/store.js
@@ -0,0 +1,38 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    socket: {
+      isConnected: false,
+      message: '',
+      reconnectError: false,
+    },
+  },
+  mutations: {
+    SOCKET_ONOPEN(state, event) {
+      Vue.prototype.$socket = event.currentTarget;
+      state.socket.isConnected = true;
+    },
+    SOCKET_ONCLOSE(state, event) {
+      state.socket.isConnected = false;
+    },
+    SOCKET_ONERROR(state, event) {
+      console.error(state, event);
+    },
+    // default handler called for all methods
+    SOCKET_ONMESSAGE(state, message) {
+      state.socket.message = message;
+    },
+    // mutations for reconnect methods
+    SOCKET_RECONNECT(state, count) {
+      console.info(state, count);
+    },
+    SOCKET_RECONNECT_ERROR(state) {
+      state.socket.reconnectError = true;
+    },
+  },
+  actions: {},
+});
