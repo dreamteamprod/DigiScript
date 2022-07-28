c86462c3a68dcad445bef02f8db37a596817a690
Tim Bradgate <timbradgate@hotmail.co.uk>
2022-07-29 00:06:47 +0100

    Add basic framework for show config view

---
+++ client/src/views/show/ShowConfigView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="show">
+    <h1>{{ this.$store.state.currentShow["name"] }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShowView',
+};
+</script>
+
+<style scoped>
+
+</style>
