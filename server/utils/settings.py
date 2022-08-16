2e4948490cf3d7d17f0c9c44b9a6f5acf8b62d5d
Tim Bradgate <timbradgate@hotmail.co.uk>
2022-08-16 20:52:00 +0100

    Watch settings file for changes, and reload

--- server/utils/settings.py
+++ server/utils/settings.py
@@ -1,12 +1,21 @@
+from __future__ import annotations
+
 import json
 import os
+from typing import TYPE_CHECKING
+
 from tornado.locks import Lock
 
 from utils.logger import get_logger
+from utils.file_watcher import IOLoopFileWatcher
+
+if TYPE_CHECKING:
+    from server.server.app_server import DigiScriptServer
 
 
 class Settings:
-    def __init__(self, settings_path=None):
+    def __init__(self, application: DigiScriptServer, settings_path=None):
+        self._application = application
         self.lock = Lock()
         self._base_path = os.path.join(os.path.dirname(__file__), '..', 'conf')
         if settings_path:
@@ -17,6 +26,20 @@ class Settings:
                 f'No settings path provided, using {self.settings_path}')
         self.settings = {}
         self._load()
+
+        self._file_watcher = IOLoopFileWatcher(self.settings_path, self.auto_reload_changes, 100)
+        self._file_watcher.watch()
+
+    def auto_reload_changes(self):
+        get_logger().info('Settings file changed; auto reloading')
+        self._load()
+
+        for client in self._application.clients:
+            client.write_message({
+                'OP': 'SETTINGS_CHANGED',
+                'DATA': self.settings,
+                'ACTION': 'WS_SETTINGS_CHANGED'
+            })
 
     def _load(self):
         if os.path.exists(self.settings_path):
@@ -30,8 +53,13 @@ class Settings:
             get_logger().info(f'Saved settings to {self.settings_path}')
 
     def _save(self):
+        self._file_watcher.pause()
         with open(self.settings_path, 'w') as fp:
             json.dump(self.settings, fp, indent=4)
+            fp.flush()
+            os.fsync(fp.fileno())
+            self._file_watcher.update_m_time()
+        self._file_watcher.resume()
         get_logger().info(f'Saved settings to {self.settings_path}')
 
     def _create_defaults(self):
