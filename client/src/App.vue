fa04aced551af9dfa8a9f6200e6de171d18f9487
Tim Bradgate <timbradgate@hotmail.co.uk>
2022-08-21 14:15:56 +0100

    Disable system and show config if websocket is unhealthy

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,8 +7,15 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item to="/config">System Config</b-nav-item>
-          <b-nav-item to="/show-config" v-if="this.$store.state.currentShow != null">
+          <b-nav-item
+            to="/config"
+            :disabled="!WEBSOCKET_HEALTHY">
+            System Config
+          </b-nav-item>
+          <b-nav-item
+            to="/show-config"
+            v-if="this.$store.state.currentShow != null"
+            :disabled="!WEBSOCKET_HEALTHY">
             Show Config
           </b-nav-item>
         </b-navbar-nav>
