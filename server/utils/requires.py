2cbe2e5f40e2160ee56f26e7e1f71b5e6de0fd76
Tim Bradgate <timbradgate@hotmail.co.uk>
2022-11-27 23:43:51 +0000

    Fix CV issues

--- server/utils/requires.py
+++ server/utils/requires.py
@@ -6,7 +6,9 @@ from tornado.web import HTTPError
 from utils.base_controller import BaseController
 
 
-def requires_show(method: Callable[..., Optional[Awaitable[None]]]) -> Callable[..., Optional[Awaitable[None]]]:
+def requires_show(
+        method: Callable[..., Optional[Awaitable[None]]]
+) -> Callable[..., Optional[Awaitable[None]]]:
     @functools.wraps(method)
     def wrapper(self: BaseController, *args, **kwargs):
         if not self.get_current_show():
