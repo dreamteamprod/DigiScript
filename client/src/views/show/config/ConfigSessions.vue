a09cb768bf38d5a2909b3afcbc7766f4626b4487
Tim Bradgate <timbradgate@hotmail.co.uk>
2023-01-13 13:40:37 +0000

    Start/stop show sessions

--- client/src/views/show/config/ConfigSessions.vue
+++ client/src/views/show/config/ConfigSessions.vue
@@ -5,16 +5,23 @@
         <h5>Sessions List</h5>
         <b-table id="acts-table" :items="this.SHOW_SESSIONS_LIST" :fields="sessionFields"
                  show-empty>
+          <template #cell(run_time)="data">
+            <p v-if="data.item.end_date_time">
+              {{ runTimeCalc(data.item.start_date_time, data.item.end_date_time) }}
+            </p>
+          </template>
         </b-table>
       </b-col>
     </b-row>
     <b-row>
       <b-col>
         <b-button-group>
-          <b-button variant="success" :disabled="CURRENT_SHOW_SESSION !== null">
+          <b-button @click.stop="startSession" variant="success"
+                    :disabled="CURRENT_SHOW_SESSION !== null || startingSession">
             Start Session
           </b-button>
-          <b-button variant="danger" :disabled="CURRENT_SHOW_SESSION === null">
+          <b-button @click.stop="stopSession" variant="danger"
+                    :disabled="CURRENT_SHOW_SESSION === null || stoppingSession">
             Stop Session
           </b-button>
         </b-button-group>
@@ -25,6 +32,9 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
+import log from 'loglevel';
+
+import {makeURL, msToTimer} from '@/js/utils';
 
 export default {
   name: 'ConfigSessions',
@@ -35,12 +45,46 @@ export default {
         { key: 'end_date_time', label: 'End Date' },
         { key: 'run_time', label: 'Runtime' },
       ],
+      startingSession: false,
+      stoppingSession: false,
     };
   },
   async mounted() {
     await this.GET_SHOW_SESSION_DATA();
   },
   methods: {
+    async startSession() {
+      this.startingSession = true;
+      const response = await fetch(`${makeURL('/api/v1/show/sessions/start')}`, {
+        method: 'POST',
+      });
+      if (response.ok) {
+        this.$toast.success('Started new show session');
+      } else {
+        log.error('Unable to start new show session');
+        this.$toast.error('Unable to start new show session');
+      }
+      this.startingSession = false;
+    },
+    async stopSession() {
+      this.stoppingSession = true;
+      const response = await fetch(`${makeURL('/api/v1/show/sessions/stop')}`, {
+        method: 'POST',
+      });
+      if (response.ok) {
+        this.$toast.success('Stopped show session');
+      } else {
+        log.error('Unable to stop show session');
+        this.$toast.error('Unable to stop show session');
+      }
+      this.stoppingSession = false;
+    },
+    runTimeCalc(start, end) {
+      const startDate = Date.parse(start);
+      const endDate = Date.parse(end);
+      const diff = endDate - startDate;
+      return msToTimer(diff);
+    },
     ...mapActions(['GET_SHOW_SESSION_DATA']),
   },
   computed: {
--- server/controllers/api/show/sessions.py
+++ server/controllers/api/show/sessions.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from models.session import ShowSession
 from models.show import Show
 from schemas.schemas import ShowSessionSchema
@@ -34,6 +36,75 @@ class SessionsController(BaseAPIController):
                 else:
                     self.set_status(404)
                     self.finish({'message': '404 show not found'})
+        else:
+            self.set_status(404)
+            self.write({'message': '404 show not found'})
+
+
+@ApiRoute('show/sessions/start', ApiVersion.v1)
+class SessionStartController(BaseAPIController):
+    @requires_show
+    async def post(self):
+        current_show = self.get_current_show()
+        show_id = current_show['id']
+        if show_id:
+            with self.make_session() as session:
+                show = session.query(Show).get(show_id)
+                if show:
+                    if show.current_session_id:
+                        self.set_status(409)
+                        await self.finish({'message': '409 session already active'})
+                    else:
+                        show_session = ShowSession(
+                            show_id=show_id,
+                            start_date_time=datetime.utcnow(),
+                            end_date_time=None
+                        )
+                        session.add(show_session)
+                        session.flush()
+
+                        show.current_session_id = show_session.id
+                        session.commit()
+
+                        self.set_status(200)
+                        self.write({'message': 'Successfully started show session'})
+
+                        await self.application.ws_send_to_all('NOOP', 'GET_SHOW_SESSION_DATA', {})
+                else:
+                    self.set_status(404)
+                    await self.finish({'message': '404 show not found'})
+        else:
+            self.set_status(404)
+            self.write({'message': '404 show not found'})
+
+
+@ApiRoute('show/sessions/stop', ApiVersion.v1)
+class SessionStopController(BaseAPIController):
+    @requires_show
+    async def post(self):
+        current_show = self.get_current_show()
+        show_id = current_show['id']
+        if show_id:
+            with self.make_session() as session:
+                show = session.query(Show).get(show_id)
+                if show:
+                    if not show.current_session_id:
+                        self.set_status(409)
+                        await self.finish({'message': '409 no active session'})
+                    else:
+                        show_session: ShowSession = session.query(ShowSession).get(
+                            show.current_session_id)
+                        show_session.end_date_time = datetime.utcnow()
+                        show.current_session_id = None
+                        session.commit()
+
+                        self.set_status(200)
+                        self.write({'message': 'Successfully stopped show session'})
+
+                        await self.application.ws_send_to_all('NOOP', 'GET_SHOW_SESSION_DATA', {})
+                else:
+                    self.set_status(404)
+                    await self.finish({'message': '404 show not found'})
         else:
             self.set_status(404)
             self.write({'message': '404 show not found'})
