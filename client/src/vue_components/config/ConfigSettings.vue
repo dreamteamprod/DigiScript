d447535314186a49ea5b60821ac0cb81b1664c2f
Tim Bradgate <timbradgate@hotmail.co.uk>
2023-01-21 00:42:18 +0000

    Enable settings to be changed from the UI

--- client/src/vue_components/config/ConfigSettings.vue
+++ client/src/vue_components/config/ConfigSettings.vue
@@ -2,19 +2,30 @@
   <b-container class="mx-0" fluid>
     <b-row>
       <b-col>
-        <template v-if="Object.keys(RAW_SETTINGS).length > 0">
-          <b-form>
-            <b-form-group v-for="(setting, key) in editSettings" :key="key"
-                          :id="`${key}-input-group`" :label="key" :label-for="`${key}-input`"
-                          :label-cols="true">
-              <b-form-input v-if="setting.type !== 'bool'"
-                            :id="`${key}-input`" :name="`${key}-input`"
-                            v-model="editSettings[key].value"
-                            :readonly="!setting.can_edit" />
-              <b-form-checkbox v-else :id="`${key}-input`" :name="`${key}-input`"
-                               v-model="editSettings[key].value"
-                               :disabled="!setting.can_edit" />
-            </b-form-group>
+        <template v-if="loaded">
+          <b-form @submit.stop.prevent="handleSubmit" @reset.stop.prevent="resetForm"
+                  :key="`settings-${toggle}`">
+            <div>
+              <b-form-group v-for="(setting, key) in RAW_SETTINGS" :key="key"
+                            :id="`${key}-input-group`" :label="key" :label-for="`${key}-input`"
+                            :label-cols="true">
+                <b-form-input v-if="setting.type !== 'bool'"
+                              :id="`${key}-input`" :name="`${key}-input`"
+                              :type="inputType(setting.type)"
+                              v-model="$v.editSettings[key].$model"
+                              :state="validateState(key)"
+                              :readonly="!setting.can_edit"
+                              :number="setting.type === 'int'" />
+                <b-form-checkbox v-else
+                                 :id="`${key}-input`" :name="`${key}-input`"
+                                 v-model="$v.editSettings[key].$model"
+                                 :disabled="!setting.can_edit" :switch="true" />
+              </b-form-group>
+              <b-button-group size="md" style="float: right">
+                <b-button type="reset" variant="danger">Reset</b-button>
+                <b-button type="submit" variant="primary">Submit</b-button>
+              </b-button-group>
+            </div>
           </b-form>
         </template>
         <div class="text-center center-spinner" v-else>
@@ -27,16 +38,83 @@
 
 <script>
 import { mapGetters } from 'vuex';
+import { required, integer } from 'vuelidate/lib/validators';
+import log from 'loglevel';
+
+import { makeURL } from '@/js/utils';
 
 export default {
   name: 'ConfigSettings',
   data() {
     return {
-      editSettings: null,
+      loaded: false,
+      editSettings: {},
+      toggle: 0,
     };
   },
   mounted() {
-    this.editSettings = JSON.parse(JSON.stringify(this.RAW_SETTINGS));
+    Object.keys(this.RAW_SETTINGS).forEach(function (x) {
+      this.editSettings[x] = this.RAW_SETTINGS[x].value;
+    }, this);
+    this.loaded = true;
+  },
+  watch: {
+    RAW_SETTINGS() {
+      this.resetForm();
+    },
+  },
+  validations() {
+    const editSettings = {};
+    Object.keys(this.editSettings).forEach((x) => {
+      if (this.RAW_SETTINGS[x].type === 'int') {
+        editSettings[x] = {
+          required,
+          integer,
+        };
+      } else {
+        editSettings[x] = {};
+      }
+    }, this);
+    return { editSettings };
+  },
+  methods: {
+    inputType(fieldType) {
+      const mapping = {
+        int: 'number',
+        str: 'text',
+      };
+      if (!Object.keys(mapping).includes(fieldType)) {
+        return 'text';
+      }
+      return mapping[fieldType];
+    },
+    validateState(name) {
+      const { $dirty, $error } = this.$v.editSettings[name];
+      return $dirty ? !$error : null;
+    },
+    async handleSubmit() {
+      const response = await fetch(`${makeURL('/api/v1/settings')}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(this.editSettings),
+      });
+      if (!response.ok) {
+        this.$toast.error('Unable to save settings');
+        log.error('Unable to save settings');
+      } else {
+        this.$toast.success('Saved settings');
+      }
+    },
+    resetForm() {
+      this.loaded = false;
+      this.toggle = !this.toggle;
+      Object.keys(this.RAW_SETTINGS).forEach(function (x) {
+        this.editSettings[x] = this.RAW_SETTINGS[x].value;
+      }, this);
+      this.loaded = true;
+    },
   },
   computed: {
     ...mapGetters(['RAW_SETTINGS']),
