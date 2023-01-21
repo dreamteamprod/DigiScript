2ca10d2f646215d50f9d9123f1ea1537dfc4b4ad
Tim Bradgate <timbradgate@hotmail.co.uk>
2023-01-21 00:41:42 +0000

    Fetch raw settings when websocket receives new settings

--- client/src/store/modules/system.js
+++ client/src/store/modules/system.js
@@ -21,16 +21,19 @@ export default {
     },
   },
   actions: {
-    async GET_SETTINGS(context) {
-      let response = await fetch(`${makeURL('/api/v1/settings/raw')}`);
+    async GET_RAW_SETTINGS(context) {
+      const response = await fetch(`${makeURL('/api/v1/settings/raw')}`);
       if (response.ok) {
         const rawSettings = await response.json();
         await context.commit('UPDATE_RAW_SETTINGS', rawSettings);
       } else {
         log.error('Unable to get system settings');
       }
+    },
+    async GET_SETTINGS(context) {
+      await context.dispatch('GET_RAW_SETTINGS');
 
-      response = await fetch(makeURL('/api/v1/settings'));
+      const response = await fetch(makeURL('/api/v1/settings'));
       if (response.ok) {
         const settings = await response.json();
         await context.dispatch('UPDATE_SETTINGS', settings);
@@ -43,6 +46,8 @@ export default {
       await context.dispatch('SETTINGS_CHANGED');
     },
     async SETTINGS_CHANGED(context) {
+      await context.dispatch('GET_RAW_SETTINGS');
+
       if (context.state.settings.current_show) {
         const currShow = context.state.settings.current_show;
         if (!context.state.currentShow || context.state.currentShow.id !== currShow) {
