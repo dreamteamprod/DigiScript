d9570958340fafdc29b4eab3891067ecc545e183
Tim Bradgate <timbradgate@hotmail.co.uk>
2023-02-01 23:51:30 +0000

    Add login/logout functionality

--- client/src/store/modules/user.js
+++ client/src/store/modules/user.js
@@ -28,6 +28,48 @@ export default {
         Vue.$toast.error('Unable to create user');
       }
     },
+    async USER_LOGIN(context, user) {
+      const response = await fetch(`${makeURL('/api/v1/auth/login')}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+      });
+      if (response.ok) {
+        context.dispatch('GET_CURRENT_USER');
+        Vue.$toast.success('Successfully logged in!');
+        return true;
+      }
+      const responseBody = await response.json();
+      log.error('Unable to log in');
+      Vue.$toast.error(`Unable to log in! ${responseBody.message}.`);
+      return false;
+    },
+    async USER_LOGOUT(context) {
+      const response = await fetch(`${makeURL('/api/v1/auth/logout')}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (response.ok) {
+        await context.commit('SET_CURRENT_USER', null);
+        Vue.$toast.success('Successfully logged out!');
+      } else {
+        log.error('Unable to log out');
+        Vue.$toast.error('Unable to log out!');
+      }
+    },
+    async GET_CURRENT_USER(context) {
+      const response = await fetch(`${makeURL('/api/v1/auth')}`);
+      if (response.ok) {
+        const user = await response.json();
+        await context.commit('SET_CURRENT_USER', user);
+      } else {
+        log.error('Unable to get current user');
+      }
+    },
   },
   getters: {
     CURRENT_USER(state) {
