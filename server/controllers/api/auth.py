3ab54abf5251b68569a9c37df3d0494ead9c0306
Tim Bradgate <timbradgate@hotmail.co.uk>
2023-02-01 22:13:45 +0000

    Add API for creating new users (#301)
    * Add API for creating new users
    
    * Add unit tests for create user API

---
+++ server/controllers/api/auth.py
@@ -0,0 +1,76 @@
+import bcrypt
+from tornado import escape
+from tornado.ioloop import IOLoop
+
+from models.show import Show
+from models.user import User
+from utils.base_controller import BaseAPIController
+from utils.route import ApiRoute, ApiVersion
+
+
+@ApiRoute('auth/create', ApiVersion.v1)
+class UserCreateController(BaseAPIController):
+
+    async def post(self):
+        data = escape.json_decode(self.request.body)
+
+        username = data.get('username', '')
+        if not username:
+            self.set_status(400)
+            await self.finish({'message': 'Username missing'})
+            return
+
+        password = data.get('password', '')
+        if not password:
+            self.set_status(400)
+            await self.finish({'message': 'Password missing'})
+            return
+        if len(password) < 6:
+            self.set_status(400)
+            await self.finish({'message': 'Password must be at least 6 characters long'})
+            return
+
+        show_id = data.get('show_id', None)
+        is_admin = data.get('is_admin', False)
+
+        if not show_id and not is_admin:
+            self.set_status(400)
+            await self.finish({'message': 'Non admin user requires a show allocation'})
+            return
+
+        if is_admin and show_id:
+            self.set_status(400)
+            await self.finish({'message': 'Admin user cannot have a show allocation'})
+            return
+
+        with self.make_session() as session:
+            if show_id:
+                if not session.query(Show).get(show_id):
+                    self.set_status(400)
+                    await self.finish({'message': 'Show not found'})
+                    return
+
+                conflict_user = session.query(User).filter(
+                    User.show_id == show_id, User.username == username).first()
+
+                if conflict_user:
+                    self.set_status(400)
+                    await self.finish({'message': 'Username already taken for this show'})
+                    return
+
+            hashed_password = await IOLoop.current().run_in_executor(
+                None, bcrypt.hashpw, escape.utf8(password), bcrypt.gensalt())
+            hashed_password = escape.to_unicode(hashed_password)
+
+            session.add(User(
+                username=username,
+                password=hashed_password,
+                show_id=show_id,
+                is_admin=is_admin))
+            session.commit()
+
+            if is_admin:
+                await self.application.digi_settings.set('has_admin_user', True)
+
+            self.set_status(200)
+            await self.finish({'message': 'Successfully created user'})
--- server/test/test_utils.py
+++ server/test/test_utils.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from tornado.testing import AsyncHTTPTestCase
@@ -8,10 +9,22 @@ from digi_server.app_server import DigiScriptServer
 class DigiScriptTestCase(AsyncHTTPTestCase):
 
     def get_app(self):
+        return DigiScriptServer(settings_path=self.settings_path)
+
+    def setUp(self):
         base_path = os.path.join(os.path.dirname(__file__), 'conf')
         settings_path = os.path.join(base_path, 'digiscript.json')
         self.settings_path = settings_path
-        return DigiScriptServer(settings_path=settings_path)
+
+        if not os.path.exists(os.path.dirname(self.settings_path)):
+            os.makedirs(os.path.dirname(self.settings_path))
+
+        with open(self.settings_path, 'w', encoding='UTF-8') as file_pointer:
+            json.dump({
+                'db_path': 'sqlite://'
+            }, file_pointer)
+
+        super().setUp()
 
     def tearDown(self):
         super().tearDown()
