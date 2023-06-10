4b6337024ddeca1cd8b31096deeef2674a9edff3
Tim Bradgate <timbradgate@hotmail.co.uk>
2023-06-10 01:00:20 +0100

    Add skeleton for mic config view

---
+++ client/src/views/show/config/ConfigMics.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-container
+    class="mx-0"
+    fluid
+  >
+    <b-row>
+      <b-col>
+        <b-tabs content-class="mt-3" >
+          <b-tab
+            title="Mics"
+            active
+          >
+            <p>Mics</p>
+          </b-tab>
+          <b-tab
+            title="Allocations"
+          >
+            <p>Allocations</p>
+          </b-tab>
+        </b-tabs>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'ConfigMics',
+};
+</script>
