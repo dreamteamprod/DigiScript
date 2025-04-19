b3d909eea5a8d6c6d8f3459277f6cd4845a8919b
Tim Bradgate <timbradgate@hotmail.co.uk>
2025-04-19 22:31:29 +0100

    Add barebones user settings page and routing

---
+++ client/src/views/user/Settings.vue
@@ -0,0 +1,14 @@
+<template>
+  <b-container
+    fluid
+    class="mx-0"
+  >
+    <h1>User Settings</h1>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'UserSettings',
+};
+</script>
