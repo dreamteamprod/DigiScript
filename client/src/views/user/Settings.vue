be418626fcff279907e3c793a7df5f751569cd49
Tim Bradgate <timbradgate@hotmail.co.uk>
2025-04-19 23:41:44 +0100

    Add simple about user table

--- client/src/views/user/Settings.vue
+++ client/src/views/user/Settings.vue
@@ -4,11 +4,25 @@
     class="mx-0"
   >
     <h1>User Settings</h1>
+    <b-tabs
+      pills
+      vertical
+    >
+      <b-tab
+        active
+        title="About"
+      >
+        <about-user />
+      </b-tab>
+    </b-tabs>
   </b-container>
 </template>
 
 <script>
+import AboutUserUser from '@/vue_components/user/settings/AboutUser.vue';
+
 export default {
   name: 'UserSettings',
+  components: { AboutUser: AboutUserUser },
 };
 </script>
