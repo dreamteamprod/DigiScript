7582890efcf4a8403621bc8c1dc7cec1f7c65aac
Tim Bradgate <timbradgate@hotmail.co.uk>
2025-04-30 17:07:09 +0100

    [Fix] Interval banner appears in random places (#638)

--- client/src/vue_components/show/live/ScriptLineViewer.vue
+++ client/src/vue_components/show/live/ScriptLineViewer.vue
@@ -7,6 +7,35 @@
     fluid
   >
     <b-row
+      v-if="needsIntervalBanner"
+      class="interval-header"
+    >
+      <b-col
+        cols="3"
+        class="cue-column d-flex align-items-center justify-content-center"
+      >
+        <b-button
+          v-if="isScriptLeader"
+          variant="primary"
+          @click.stop="startInterval"
+        >
+          Start Interval
+        </b-button>
+      </b-col>
+      <b-col
+        cols="9"
+        class="interval-banner"
+      >
+        <b-alert
+          show
+          variant="warning"
+          style="margin: 0"
+        >
+          <h3> {{ previousActLabel }} - Interval</h3>
+        </b-alert>
+      </b-col>
+    </b-row>
+    <b-row
       v-if="needsActSceneLabel"
       class="act-scene-header"
     >
@@ -109,35 +138,6 @@
           </b-row>
         </b-col>
       </template>
-    </b-row>
-    <b-row
-      v-if="needsIntervalBanner"
-      class="interval-footer"
-    >
-      <b-col
-        cols="3"
-        class="cue-column d-flex align-items-center justify-content-center"
-      >
-        <b-button
-          v-if="isScriptLeader"
-          variant="primary"
-          @click.stop="startInterval"
-        >
-          Start Interval
-        </b-button>
-      </b-col>
-      <b-col
-        cols="9"
-        class="interval-banner"
-      >
-        <b-alert
-          show
-          variant="warning"
-          style="margin: 0"
-        >
-          <h3> {{ actLabel }} - Interval</h3>
-        </b-alert>
-      </b-col>
     </b-row>
   </b-container>
 </template>
@@ -260,15 +260,17 @@ export default {
         && previousLine.scene_id === this.line.scene_id);
     },
     needsIntervalBanner() {
-      let [lineIndex, nextLine] = this.getNextLineForIndex(this.line.page, this.lineIndex);
-      while (nextLine != null && this.isWholeLineCut(nextLine)) {
-        [lineIndex, nextLine] = this.getNextLineForIndex(nextLine.page, lineIndex);
+      let { previousLine, lineIndex } = this;
+      while (previousLine != null && this.isWholeLineCut(previousLine)) {
+        [lineIndex, previousLine] = this.getPreviousLineForIndex(previousLine.page, lineIndex);
       }
-      if (nextLine == null) {
-        return this.acts.find((act) => (act.id === this.line.act_id)).interval_after;
+      if (previousLine == null) {
+        return true;
       }
-      return (this.line.act_id !== nextLine.act_id
-        && this.acts.find((act) => (act.id === nextLine.act_id)).interval_after);
+      return previousLine.act_id !== this.line.act_id;
+    },
+    previousActLabel() {
+      return this.acts.find((act) => (act.id === this.previousLine.act_id)).name;
     },
     actLabel() {
       return this.acts.find((act) => (act.id === this.line.act_id)).name;
@@ -365,27 +367,12 @@ export default {
       }
       return [null, null];
     },
-    getNextLineForIndex(pageIndex, lineIndex) {
-      const currentPage = this.GET_SCRIPT_PAGE(pageIndex);
-      if (lineIndex < currentPage.length - 1) {
-        return [lineIndex + 1, currentPage[lineIndex + 1]];
-      }
-      let loopPageNo = pageIndex + 1;
-      while (loopPageNo <= this.currentMaxPage) {
-        const loopPage = this.GET_SCRIPT_PAGE(loopPageNo);
-        if (loopPage && loopPage.length > 0) {
-          return [0, loopPage[0]];
-        }
-        loopPageNo += 1;
-      }
-      return [null, null];
-    },
     isWholeLineCut(line) {
       return line.line_parts.every((linePart) => (this.SCRIPT_CUTS.includes(linePart.id)
           || linePart.line_text == null || linePart.line_text.trim().length === 0), this);
     },
     startInterval() {
-      this.$emit('start-interval', this.acts.find((act) => (act.id === this.line.act_id)).id);
+      this.$emit('start-interval', this.acts.find((act) => (act.id === this.previousLine.act_id)).id);
     },
     addNewCue() {
       this.$emit('add-cue', this.line.id);
@@ -426,10 +413,11 @@ export default {
     background: #3498db54;
   }
 
-  .interval-footer, .act-scene-header {
+  .interval-header, .act-scene-header {
     background: var(--body-background);
   }
-  .interval-footer {
-    margin-bottom: 1rem;
+  .interval-header {
+    margin-top: 1rem;
+    padding-bottom: 1rem;
   }
 </style>
