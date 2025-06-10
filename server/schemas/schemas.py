19279b184a596e23429f3aa472b7c0382122202e
Tim Bradgate <timbradgate@hotmail.co.uk>
2025-06-10 22:43:15 +0100

    Add compiled script to front and backend

--- server/schemas/schemas.py
+++ server/schemas/schemas.py
@@ -1,11 +1,8 @@
-from typing import Optional
-
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, SQLAlchemySchema, auto_field
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, auto_field
 from marshmallow_sqlalchemy.fields import Nested
 
 from models.cue import Cue, CueType
 from models.mics import Microphone, MicrophoneAllocation
-from models.models import db
 from models.script import (
     Script,
     ScriptCuts,
@@ -17,29 +14,7 @@ from models.script import (
 from models.session import Interval, Session, ShowSession
 from models.show import Act, Cast, Character, CharacterGroup, Scene, Show
 from models.user import User, UserSettings
-
-
-class SchemaRegistry:
-    def __init__(self):
-        self._forward_registry = {}
-        self._backward_registry = {}
-
-    def set(self, key, value):
-        self._forward_registry[key] = value
-        self._backward_registry[value] = key
-
-    def get_schema_by_model(self, key) -> Optional[SQLAlchemySchema]:
-        return self._backward_registry.get(key, None)
-
-    def get_model_by_schema(self, key) -> db.Model:
-        return self._forward_registry.get(key, None)
-
-
-registry = SchemaRegistry()
-
-
-def get_registry():
-    return registry
+from registry.schema import get_registry
 
 
 def schema(cls):
