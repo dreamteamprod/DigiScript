ff2886821ce2daf73f46c135cd53ee3523df1e38
Tim Bradgate <timbradgate@hotmail.co.uk>
2025-06-23 00:32:48 +0100

    Add basic outline for stage manager tab

--- client/src/views/show/config/ConfigStage.vue
+++ client/src/views/show/config/ConfigStage.vue
@@ -18,6 +18,9 @@
           <b-tab title="Props">
             <props-list />
           </b-tab>
+          <b-tab title="Stage Manager">
+            <stage-manager />
+          </b-tab>
         </b-tabs>
       </b-col>
     </b-row>
@@ -28,9 +31,12 @@
 import CrewList from '@/vue_components/show/config/stage/CrewList.vue';
 import SceneryList from '@/vue_components/show/config/stage/SceneryList.vue';
 import PropsList from '@/vue_components/show/config/stage/PropsList.vue';
+import StageManager from '@/vue_components/show/config/stage/StageManager.vue';
 
 export default {
   name: 'ConfigCrew',
-  components: { PropsList, SceneryList, CrewList },
+  components: {
+    StageManager, PropsList, SceneryList, CrewList,
+  },
 };
 </script>
